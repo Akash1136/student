e0a80442b1ce2995c6f38c3a59474446dfaaa9f9
Akash1136 <pate3576@sheridancollege.ca>
2022-01-19 13:59:14 -0500

    First Commit

---
+++ src/studentList/studentList.java
@@ -0,0 +1,18 @@
+
+package studentList;
+
+import Student.Student;
+
+public class studentList {
+    public static void main(String[] args){
+        Student[] studentList = new Student[3];
+        studentList[0] = new Student("1","Ronak Sheth");
+        studentList[1] = new Student("2","John Doe");
+        studentList[2] = new Student("3","abc");
+        
+        for(int  i = 0;i<studentList.length;i++){
+            System.out.println(studentList[i].getStudentName());
+        }
+        
+    }
+}
